63174e771d85c704c20771866ca4e67253bd6ac5
Mike Kwiatkowsky <mikeski885@gmail.com>
2026-08-16 20:31:13 -0400

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+
+const Nav = () => {
+  return (
+    <nav>
+      <div>
+        <div className="nav__container">
+          <a href="/">
+            <img src="" alt="" className="logo" />
+          </a>
+          <ul className="nav__links">
+            <li className="nav__list">
+              <a href="/" className="nav__link">
+                Home
+              </a>
+            </li>
+            <li className="nav__list">
+              <a href="/" className="nav__link">
+                Books
+              </a>
+            </li>
+            <button className="btn__menu">
+            </button>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
