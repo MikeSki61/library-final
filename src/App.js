3d660bd107e5a50557ad134218a0ddd0a934c5e9
Mike Kwiatkowsky <mikeski885@gmail.com>
2026-09-04 19:46:23 -0400

    added cart page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import Nav from './components/Nav';
+import React, { useState, useEffect } from 'react';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Books from './pages/Books'
 import { books } from './data';
 import BookInfo from './pages/BookInfo'
-
+import Cart from './pages/Cart';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart])
+
+
   return (
     <Router>
       <div className="App">
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/books" element={<Books books={books} /> }/>
-          <Route path="/books/1" element={<BookInfo books={books} /> }/>
+          <Route path="/books" element={<Books books={books} />} />
+          <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart} />} />
+          <Route path="/cart" element={<Cart books={books} />} />
         </Routes>
         <Footer />
       </div>
